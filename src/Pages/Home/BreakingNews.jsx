70ad7e40971c3c9c59fb3e3d414f31a064b691a0
Mohammad <mohammadrasel201@gmail.com>
2023-12-27 08:13:11 +0600

    Added Left and Right Sidebars

---
+++ src/Pages/Home/BreakingNews.jsx
@@ -0,0 +1,18 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
+
+const BreakingNews = () => {
+    return (
+        <div className=" flex">
+            <button className=" btn btn-secondary">Breaking News</button>
+            <Marquee pauseOnHover={true} speed={80}>
+                <Link className="mr-12" to="/">
+                Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
+                </Link>
+            </Marquee>
+        </div>
+    );
+};
+
+export default BreakingNews;
